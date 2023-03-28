3f5b0d93b19b15471643e421b9f7f8380d97a318
sanjitha.siva <sanjitha.siva@aspiresys.com>
2023-03-28 18:31:01 +0530

    correct

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-form',
@@ -7,9 +8,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FormComponent implements OnInit {
 
-  constructor() { }
+  title = 'templateDriven';
 
+  fname:string='';
+  lname:string='';
+  terms:boolean=false;
+  gender:string='';
+  address:string='';
+  
+  addList(formData:NgForm){
+    console.log(formData.value);
+  }
+
+  resetValues(formData:NgForm){
+    formData.reset();
+  }
+
+  constructor(){
+
+  }
   ngOnInit(): void {
+
+  }
+
+  setValues(formData:NgForm){
+    let userDetails={
+      fname:'Sanju',
+      lname:'Siva',
+      terms:false,
+      gender:'',
+      address:'Coimbatore'
+    }
+    formData.setValue(userDetails);
+  }
+
+  display=false;
+  displayValues(){
+    this.display=!this.display;
+    
   }
 
 }
